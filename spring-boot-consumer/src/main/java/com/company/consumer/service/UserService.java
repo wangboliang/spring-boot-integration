73af7707a6417db482c3aa2061cc964e17fef995
wangliang <wang.liang@vpclub.cn>
2017-07-08 14:59:03 +0800

    spring-boot-integration

---
+++ spring-boot-consumer/src/main/java/com/company/consumer/service/UserService.java
@@ -0,0 +1,116 @@
+package com.company.consumer.service;
+
+import com.company.consumer.entity.User;
+import com.company.consumer.enums.ReturnCodeEnum;
+import com.company.consumer.model.request.UserPageParam;
+import com.company.consumer.model.response.BaseResponse;
+import com.company.consumer.model.response.PageDataResponse;
+import com.company.consumer.model.response.PageResponse;
+import com.company.consumer.rpc.UserRpcService;
+import com.company.consumer.utils.BackResponseUtil;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+;
+
+/**
+ * <p>
+ * service业务层
+ * </p>
+ *
+ * @author wangliang
+ * @since 2017-07-05
+ */
+@Slf4j
+@Service
+@AllArgsConstructor
+public class UserService {
+
+    private UserRpcService userRpcService;
+
+    public BaseResponse add(User request) {
+        BaseResponse response;
+        //业务操作
+        log.info("add User " + request);
+        try {
+            response = userRpcService.add(request);
+        } catch (Exception e) {
+            log.error("error: {}", e);
+            response = BackResponseUtil.getBaseResponse(ReturnCodeEnum.CODE_1004.getCode());
+            response.setMessage(ReturnCodeEnum.CODE_1004.getValue());
+        }
+        return response;
+    }
+
+    public BaseResponse update(User request) {
+        BaseResponse response;
+        //业务操作
+        log.info("update User " + request);
+        try {
+            response = userRpcService.update(request);
+            log.debug("update User back " + response);
+        } catch (Exception e) {
+            log.error("error: {}", e);
+            response = BackResponseUtil.getBaseResponse(ReturnCodeEnum.CODE_1004.getCode());
+            response.setMessage(ReturnCodeEnum.CODE_1004.getValue());
+        }
+        return response;
+    }
+
+    public BaseResponse query(User request) {
+        BaseResponse response;
+        //业务操作
+        log.info("query User " + request);
+        try {
+            response = userRpcService.query(request);
+            log.info("query User back" + response);
+        } catch (Exception e) {
+            log.error("error: {}", e);
+            response = BackResponseUtil.getBaseResponse(ReturnCodeEnum.CODE_1004.getCode());
+            response.setMessage(ReturnCodeEnum.CODE_1004.getValue());
+        }
+        return response;
+    }
+
+    public BaseResponse delete(User request) {
+        BaseResponse response;
+        //业务操作
+        log.info("delete User " + request);
+        try {
+            response = userRpcService.delete(request);
+            log.debug("delete User back " + response);
+        } catch (Exception e) {
+            log.error("error: {}", e);
+            response = BackResponseUtil.getBaseResponse(ReturnCodeEnum.CODE_1004.getCode());
+            response.setMessage(ReturnCodeEnum.CODE_1004.getValue());
+        }
+        return response;
+    }
+
+    public PageDataResponse list(UserPageParam request) {
+        PageDataResponse response;
+        //业务操作
+        log.info("User list method request " + request);
+        if (null == request) {
+            response = BackResponseUtil.getPageDataResponse(ReturnCodeEnum.CODE_1004.getCode());
+        } else {
+            log.info("User list method request " + request);
+            response = userRpcService.list(request);
+        }
+        return response;
+    }
+
+    public PageResponse page(UserPageParam request) {
+        PageResponse response;
+        //业务操作
+        log.info("User page method request " + request);
+        if (null == request) {
+            response = BackResponseUtil.getPageResponse(ReturnCodeEnum.CODE_1004.getCode());
+        } else {
+            log.info("User page method request " + request);
+            response = userRpcService.page(request);
+        }
+        return response;
+    }
+}
---
+++ spring-boot-consumer/src/test/java/com/company/consumer/service/UserServiceTests.java
@@ -0,0 +1,10 @@
+package com.company.consumer.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@Slf4j
+public class UserServiceTests {
+
+}
