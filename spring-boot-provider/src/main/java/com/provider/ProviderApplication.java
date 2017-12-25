b41068b236b8925372cdb19cb4415102d6ca0a18
wangboliang <bright.wbl@gmail.com>
2017-12-25 17:25:16 +0800

    add @MapperScan

--- spring-boot-provider/src/main/java/com/provider/ProviderApplication.java
+++ spring-boot-provider/src/main/java/com/provider/ProviderApplication.java
@@ -1,8 +1,10 @@
 package com.provider;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan("com.provider.mapper")
 @SpringBootApplication
 public class ProviderApplication {
 
